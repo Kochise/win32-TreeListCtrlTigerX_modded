b34302b199c92e2e47a0d3f513fc22d8c2dd0318
Kochise <david.koch@9online.fr>
2016-12-19 20:31:26 +0100

    As of 060120 (yes, 2006)
    Upload the legacy software

---
+++ TurboDemo/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by TurboDemo.rc
+//
+#define IDD_TURBODEMO_DIALOG            102
+#define IDD_TURBODEMO_DIALOG1           103
+#define IDD_TURBODRAG_DIALOG            103
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP_TREE                 129
+#define IDC_BUTTON_DD                   1000
+#define IDC_BUTTON_REDO                 1001
+#define IDC_STATIC_DELETE               1002
+#define IDC_STATIC_INSERT               1003
+#define IDC_STATIC_EXPAND               1004
+#define IDC_TREELIST                    1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
